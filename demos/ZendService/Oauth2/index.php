4b71d121115dadcd2bd000c04f8fba1ccda69b3d
Jurgen Van de Moere <jurgen@webberid.com>
2013-03-01 14:08:11 +0100

    Added basic demo page

---
+++ demos/ZendService/Oauth2/index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Service
+ */
+
+include_once '_autoload.php';
+
+use ZendService\Oauth2\Client\Client as Client;
+
+try{
+?>
+    <?php
+    $client = new Client(array(
+        'client_id' => '12345',
+    ));
+    ?>
+    
+    <h4>Client</h4>
+    <pre>
+    <?php var_dump($client) ?>
+    </pre>
+    
+    <h4>Authorization Grant</h4>
+    <pre>
+    <?php var_dump($client->getAuthorizationGrant()); ?>
+    </pre>
+
+<?php
+} catch(\Exception $e) {
+?>
+<h4>Exception</h4>
+<pre><?php var_dump($e) ?></pre>
+<?php
+}
+?>
