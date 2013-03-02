301d02b00e947413276b387d4febc124e5a1bcb3
Jurgen Van de Moere <jurgen@webberid.com>
2013-03-02 23:56:20 +0100

    Refactored to make use of Zend\Http\Client instead of \Zend\Http\ClientStatic

--- library/ZendService/Oauth2/Http/Client/Client.php
+++ library/ZendService/Oauth2/Http/Client/Client.php
@@ -10,14 +10,17 @@
  *
  * HTTP client used to perform the HTTP requests
  *
- * Delegates to Zend\Http\ClientStatic
+ * Delegates to Zend\Http\Client
  *
  */
 
 namespace ZendService\Oauth2\Http\Client;
 
+
+use Zend\Http\Client as ZendHttpClient;
+use Zend\Http\Request as ZendHttpRequest;
 use ZendService\Oauth2\Http\Client\ClientInterface;
-use Zend\Http\ClientStatic;
+use ZendService\Oauth2\Http\Exception\Exception;
 
 /**
  * @category   Zend
@@ -28,36 +31,41 @@ class Client implements ClientInterface
 {
     
     /**
-     * Placeholder for static HTTP client
+     * Placeholder for Zend Http client
      * @var Zend\Http\ClientStatic
      */
-    protected $_clientStatic = null;
+    protected $_zendHttpClient = null;
     
     /**
-     * Get static HTTP client
+     * Placeholder for options that are passed to the constructor
      *
-     * @return \ZendService\Oauth2\Http\Zend\Http\ClientStatic
+     * These options are passed to the http client constructor
+     *
+     * @var mixed
      */
-    public function getClientStatic()
+    protected $_options = null;
+    
+    /**
+     * Constructor
+     *
+     * @param mixed $options Options that need to be passed to authorization grant
+     */
+    public function __construct($options = array())
     {
-        if(null === $this->_clientStatic) {
-            $this->_clientStatic = new ClientStatic();
-        }
-        return $this->_clientStatic;
+        $this->_options = $options;
     }
     
     /**
-     * Set static HTTP client
+     * Get zend HTTP client
      *
-     * @param Zend\Http\ClientStatic $clientStatic
-     * @return self
+     * @return \ZendService\Oauth2\Http\Zend\Http\ClientStatic
      */
-    public function setClientStatic($clientStatic)
+    protected function _getZendHttpClient()
     {
-        if($clientStatic instanceof ClientStatic) {
-            $this->_clientStatic = $clientStatic;
+        if(null === $this->_zendHttpClient) {
+            $this->_zendHttpClient = new ZendHttpClient($this->_options);
         }
-        return $this;
+        return $this->_zendHttpClient;
     }
     
     /**
@@ -67,8 +75,29 @@ class Client implements ClientInterface
     public function get($url, $query = array(), $headers = array(), $body = null)
     {
         
-        // Delegate to static HTTP client
-        return $this->getClientStatic()->get($url, $query, $headers, $body);
+        // Handle invalid URL
+        if (empty($url)) {
+            return false;
+        }
+
+        // Build request
+        $request= new ZendHttpRequest();
+        $request->setUri($url);
+        $request->setMethod(ZendHttpRequest::METHOD_GET);
+
+        if (!empty($query) && is_array($query)) {
+            $request->getQuery()->fromArray($query);
+        }
+
+        if (!empty($headers) && is_array($headers)) {
+            $request->getHeaders()->addHeaders($headers);
+        }
+
+        if (!empty($body)) {
+            $request->setBody($body);
+        }
+
+        return $this->_getZendHttpClient()->send($request);
     }
     
     /**
@@ -77,8 +106,35 @@ class Client implements ClientInterface
      */
     public function post($url, $params, $headers = array(), $body = null)
     {
-        
-        // Delegate to static HTTP client
-        return $this->getClientStatic()->post($url, $params, $headers, $body);
+
+        // Handle invalid URL
+        if (empty($url)) {
+            return false;
+        }
+
+        // Build request
+        $request= new ZendHttpRequest();
+        $request->setUri($url);
+        $request->setMethod(ZendHttpRequest::METHOD_POST);
+
+        if (!empty($params) && is_array($params)) {
+            $request->getPost()->fromArray($params);
+        } else {
+            throw new Exception('The array of post parameters is empty');
+        }
+
+        if (!isset($headers['Content-Type'])) {
+            $headers['Content-Type']= ZendHttpClient::ENC_URLENCODED;
+        }
+
+        if (!empty($headers) && is_array($headers)) {
+            $request->getHeaders()->addHeaders($headers);
+        }
+
+        if (!empty($body)) {
+            $request->setContent($body);
+        }
+
+        return $this->_getZendHttpClient()->send($request);
     }
 }
