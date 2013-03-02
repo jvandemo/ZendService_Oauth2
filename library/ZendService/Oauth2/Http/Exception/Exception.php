eea10e633f02c36f38fa3b4389e40dbdc4abdb25
Jurgen Van de Moere <jurgen@webberid.com>
2013-03-02 23:58:26 +0100

    Added ZendService\Oauth2\Http\Exception classes

---
+++ library/ZendService/Oauth2/Http/Exception/Exception.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   ZendService_Oauth2
+ */
+
+namespace ZendService\Oauth2\Http\Exception;
+
+/**
+ * Oauth2 exception
+ *
+ * @category   Zend
+ * @package    ZendService_Oauth2
+ */
+class Exception extends \Exception implements ExceptionInterface
+{
+}
